4c500c0d66e9a804efe95a0fced2a9c209899324
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-04-21 18:20:46 +0530

    added: contact creation

--- main/java/com/addressbookmgmt/AddressBookMain.java
+++ main/java/com/addressbookmgmt/AddressBookMain.java
@@ -3,5 +3,6 @@ package com.addressbookmgmt;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome in address book management");
+        Contact contact = new Contact("murari","kumar","mokama","patna","bihar","4566755","8709628464","murari05@gmail.com");
     }
 }
